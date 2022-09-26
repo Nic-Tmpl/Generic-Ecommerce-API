110da3c6bd6d4120030c7836f041f1249a3870c4
Nic-Temple <nictemplepiano@gmail.com>
2022-09-26 16:27:59 -0700

    adds dotenv and create db directory with index file

---
+++ db/index.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } =require('pg')
+
+const pool = new Pool()
+
+module.exports = {
+    query: (text, params, callback) => {
+        return pool.query(text, params, callback)
+    },
+}
