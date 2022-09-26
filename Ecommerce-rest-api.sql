d154ffcd1f80b40f42e91d64353ac61e86acebcd
Nic-Temple <nictemplepiano@gmail.com>
2022-09-26 16:13:00 -0700

    adds node-postgres connection

---
+++ Ecommerce-rest-api.sql
@@ -0,0 +1,65 @@
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "password" varchar,
+  "email" varchar,
+  "first_name" varchar,
+  "last_name" varchar,
+  "created" timestamp,
+  "modified" timestamp
+);
+
+CREATE TABLE "order" (
+  "id" int UNIQUE PRIMARY KEY,
+  "user_id" int,
+  "status" varchar,
+  "total" decimal,
+  "created" timestamp,
+  "modified" timestamp
+);
+
+CREATE TABLE "order_item" (
+  "order_id" int,
+  "product_id" int,
+  "quantity" int
+);
+
+CREATE TABLE "categories" (
+  "id" int PRIMARY KEY,
+  "name" varchar
+);
+
+CREATE TABLE "products" (
+  "id" int PRIMARY KEY,
+  "name" varchar,
+  "description" text,
+  "category" int,
+  "price" decimal
+);
+
+CREATE TABLE "cart" (
+  "id" int PRIMARY KEY,
+  "user_id" int,
+  "total" decimal,
+  "created" timestamp,
+  "modified" timestamp
+);
+
+CREATE TABLE "cart_item" (
+  "cart_id" int,
+  "product_id" int,
+  "quantity" int
+);
+
+ALTER TABLE "order" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "order_item" ADD FOREIGN KEY ("order_id") REFERENCES "order" ("id");
+
+ALTER TABLE "order_item" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
+
+ALTER TABLE "products" ADD FOREIGN KEY ("category") REFERENCES "categories" ("id");
+
+ALTER TABLE "cart" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "cart_item" ADD FOREIGN KEY ("cart_id") REFERENCES "cart" ("id");
+
+ALTER TABLE "cart_item" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
