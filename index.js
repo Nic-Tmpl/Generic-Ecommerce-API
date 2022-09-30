b5827b693ee1213b28247d5865c7c2ac208bd66a
Nic-Temple <nictemplepiano@gmail.com>
2022-09-29 19:04:48 -0700

    Adds connect-pg-simple for session data in psql

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const mountRoutes = require('./routes/index');
 const  app = express();
 mountRoutes(app);
 
+//psql session store
+const store = new(require('connect-pg-simple')(session))({
+    conObject,
+});
+
 app.use(session({
+    store: store,
     secret: 'placeholder',
     resave: false,
     saveUninitialized: false,
