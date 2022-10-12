1202dbe009dd8d1e0d3562abc79e2e6598cb031a
Nic-Temple <nictemplepiano@gmail.com>
2022-10-11 18:13:29 -0700

    begins ops on orders router

--- routes/orders.js
+++ routes/orders.js
@@ -5,3 +5,15 @@ const router = new Router();
 
 //export router to be mounted in routes/index.js
 module.exports = router;
+
+router.get('/', async(req, res) => {
+    const { user_id } = req.body;
+    const { rows } = await db.query(`SELECT * FROM orders WHERE user_id = $1`, [user_id]);
+    res.send(rows);
+});
+
+router.get('/:orderId', async(req, res) => {
+    const {orderId }  = req.params
+    const { user_id } = req.body;
+    const { rows } = await db.query(`SELECT * FROM orders WHERE id = $1 and user_id = $2`, [user_id]);
+});
